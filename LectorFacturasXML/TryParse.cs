276a577be9fda34a837bd089cb0de083cd3612a7
Programador <Programador@DESKTOP-3QGLTS1>
2016-08-24 16:50:24 -0700

    Added new class that contains two methods to validate if the info. given contains  valid xml formato, and It has been removedunused dlls.

---
+++ LectorFacturasXML/TryParse.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml;
+
+namespace LectorFacturasXML
+{
+    public static class TryParse
+    {
+        /// <summary>
+        /// Metodo to validate if the xml string contains a valid formato.
+        /// </summary>
+        /// <param name="xmlString"></param>
+        /// <returns></returns>
+        public static bool TryParseFromString(string xmlString)
+        {
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(xmlString);
+                return true;
+            }
+            catch (XmlException ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Metodo to validate if the given file contains a valid formato.
+        /// </summary>
+        /// <param name="localPath"></param>
+        /// <returns></returns>
+        public static bool TryParseFromLocalFile(string localPath)
+        {
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load(localPath);
+                return true;
+            }
+            catch (XmlException ex)
+            {
+                return false;
+            }
+        }
+    }
+}
